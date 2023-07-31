696972e4461a6a2a81f78f1f04561e03705f67d1
Ryan Rizeq <ryanrizeq@gmail.com>
2023-07-30 20:43:04 -0700

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'black', 'docile', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 7, 'M', 'brown', 'energetic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 12, 'F', 'blue', 'Great', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Seargent Brown', 2, 'M', 'pink', 'free spirit', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 22, 'F', 'black', 'dead', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 18, 'M', 'green', 'mean', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 3, 'W', 'purple', 'docile', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, 'M', 'black', 'docile', 0)
